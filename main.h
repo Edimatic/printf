0245b5496c4ecf5c3b0a84955ce335f36303cfe6
babyaga0 <shsaids207@gmail.com>
2023-07-21 07:43:11 -0700

    printc

---
+++ main.h
@@ -0,0 +1,22 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdarg.h>
+int _putchar(char c);
+void _puts(char *str);
+int _strlen(char *s);
+int _printf(const char *format, ...);
+char *convert_to(char representation[], unsigned int num, int base);
+int function_manager(char c, va_list arg);
+int _switch(char c, va_list arg);
+int print_character(va_list arg);
+int print_sign(va_list arg, int base);
+int print_unsign(va_list arg, int base);
+int print_string(va_list arg);
+int print_ptr(va_list arg);
+int print_rot13(va_list arg);
+int print_rev(va_list arg);
+int print_base16_upper_lower(va_list arg, char *representation);
+int loop_format(va_list arg, const char *string);
+int call_function_manager(char aux, va_list arg);
+int check_percent(int *flag, char aux);
+#endif
